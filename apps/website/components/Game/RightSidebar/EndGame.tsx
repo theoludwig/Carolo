0f5052cb552a09a21184abbe7da15773387d19cb
Théo LUDWIG <contact@theoludwig.fr>
2023-03-11 13:24:57 +0100

    feat: improve winner visuals

--- apps/website/components/Game/RightSidebar/EndGame.tsx
+++ apps/website/components/Game/RightSidebar/EndGame.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import Image from 'next/image'
 import { getOppositePieceColor } from '@carolo/game'
 
 import { useGame } from '@/stores/game'
@@ -10,6 +11,9 @@ export const EndGame = (): JSX.Element => {
   })
   const gameState = useGame((state) => {
     return state.gameState
+  })
+  const users = useGame((state) => {
+    return state.users
   })
 
   const winnerColor = useMemo(() => {
@@ -61,12 +65,32 @@ export const EndGame = (): JSX.Element => {
     return 'Pas de vainqueur'
   }, [reason])
 
+  const winner = useMemo(() => {
+    return users.find((user) => {
+      return user.color === winnerColor
+    })
+  }, [users, winnerColor])
+
   return (
     <>
-      <h2>
+      <h2 className='text-lg font-bold'>
         {reasonDisplay}{' '}
         {winnerColorDisplay != null ? `• Victoire ${winnerColorDisplay}` : null}
       </h2>
+      {winner != null ? (
+        <div className='flex flex-row items-center justify-center space-x-2 px-4 py-2'>
+          <Image
+            className='h-12 w-12 rounded-full ring-2 ring-green-500'
+            priority
+            quality={100}
+            src={winner.logo ?? '/data/user-default.png'}
+            alt='Player Picture'
+            width={64}
+            height={64}
+          />
+          <h2 className='text-lg font-semibold'>{winner.name}</h2>
+        </div>
+      ) : null}
       <div className='mt-2'>
         <Button variant='purple' href='/game'>
           Nouvelle partie
